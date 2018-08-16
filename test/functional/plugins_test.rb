32145b332dd5e42b9c1cc5465f77aa73625657c6
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-08-16 10:38:14 -0400

    Sketch out in comments the unit and functional tests for the installer
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/plugins_test.rb
+++ test/functional/plugins_test.rb
@@ -55,6 +55,7 @@ end
 # Should be able to install a gem-based plugin
 # Should be able to list plugins when none installed
 # Should be able to list plugins when some installed
+# Should be able to list core plugins as well
 # Should be able to uninstall a gem plugin
 # Uninstalling a nonexistant plugin is a code 2 error
 
@@ -67,6 +68,15 @@ end
 # Should be able to install a v2 platform plugin
 
 # Should refuse to install gems that do not begin with train- or inspec-
+
+# Should be able to install a plugin while pinning the version
+# Should be able to upgrade a plugin
+
+# Should be able to search for available plugins
+# Should be able to search for plugins and assume the inspec- or train- prefixes.
+
+# Should be able to suggest a train transport plugin when a gem search is successful
+# Should raise an error if no train transport plugin exists
 
 #=========================================================================================#
 #                           CliCommand plugin type
